4d1ae0d9801ae796972e4beb3360f3453769c4e9
Yoosuf-Khan-APCSA <ykhan977@student.fuhsd.org>
2024-10-18 14:57:39 -0700

    Add files via upload

---
+++ WordUtils.java
@@ -0,0 +1,157 @@
+import java.util.Scanner;
+
+/**
+ *	Provides utilities for word games:
+ *	1. finds all words in the dictionary that match a list of letters
+ *	2. prints an array of words to the screen in tabular format
+ *	3. finds the word from an array of words with the highest score
+ *	4. calculates the score of a word according to a table
+ *
+ *	Uses the FileUtils and Prompt classes.
+ *	
+ *	@author 
+ *	@since	
+ */
+ 
+public class WordUtils
+{
+	private String[] words;		// the dictionary of words
+	
+	// File containing dictionary of almost 100,000 words.
+	private final String WORD_FILE = "wordList.txt";
+	
+	/* Constructor */
+	public WordUtils() {
+		loadWords();
+ }
+	
+	/**	Load all of the dictionary from a file into words array. */
+	private void loadWords () {
+		int length=0;
+		int wordListLength=0;
+		Scanner input = FileUtils.openToRead(WORD_FILE);
+		while (input.hasNext()) {
+			input.next();
+			wordListLength++;
+		}
+		input.close();
+		words=new String [wordListLength];
+		 input = FileUtils.openToRead(WORD_FILE);
+		while (input.hasNext()) {
+			words[length] = input.next();
+			length++;
+		}
+		input.close();
+		}
+	
+	/**	Find all words that can be formed by a list of letters.
+	 *  @param letters	string containing list of letters
+	 *  @return			array of strings with all words found.
+	 */
+	public String [] findAllWords (String letters)
+	{	
+
+		
+		String foundWords[];
+		int length =0,resultLength=0;
+		boolean isWordMatch=true;
+		for(int i =0; i<words.length; i++ ){
+			if(isWordMatch(words[i],letters))	length++;
+		}
+		
+		foundWords = new String [length];
+		for(int i =0; i<words.length; i++ ){
+			if(isWordMatch(words[i],letters)){
+				 foundWords[resultLength]=words[i];
+				 resultLength++;
+			 }
+		}
+		return foundWords;
+	}
+	/**
+	 *  Decides if a word matches a group of letters.
+	 *
+	 *  @param word  The word to test.
+	 *  @param letters  A string of letters to compare
+	 *  @return  true if the word matches the letters, false otherwise
+	 */
+	public boolean isWordMatch (String word, String letters) {
+		for (int a=0; a<word.length(); a++){
+			char c = word.charAt(a);
+			if ( letters.indexOf(c) > -1)
+				letters = letters.substring(0, letters.indexOf(c))
+								+letters.substring(letters.indexOf(c) + 1);
+			else 
+				return false;
+		}
+		return true;
+	}
+	/**	Print the words found to the screen.
+	 *  @param words	array containing the words to be printed
+	 */
+	public void printWords (String [] wordList) {
+		System.out.println();
+		for (int i = 0; i < wordList.length; i++) {
+			System.out.printf("%-8s", words[i]);
+			if(i%4==0)System.out.println();
+		}
+			
+		 }
+	
+	/**	Finds the highest scoring word according to a score table.
+	 *
+	 *  @param word  		An array of words to check
+	 *  @param scoreTable	An array of 26 integer scores in letter order
+	 *  @return   			The word with the highest score
+	 */
+	public String bestWord (String [] wordList, int [] scoreTable)
+	{
+		int maxScore=0;
+		String maxWord="";
+		for(int i=0; i <wordList.length; i++){
+			if(getScore(wordList[i],scoreTable)>maxScore){
+				maxScore = getScore(wordList[i],scoreTable);
+				maxWord = wordList[i];
+			}
+		}
+		return maxWord;
+	}
+	
+	/**	Calculates the score of one word according to a score table.
+	 *
+	 *  @param word			The word to score
+	 *  @param scoreTable	An array of 26 integer scores in letter order
+	 *  @return				The integer score of the word
+	 */
+	public int getScore (String word, int [] scoreTable)
+	{
+		int score=0;
+		for(int i =0; i<word.length();i++){
+			score+=scoreTable[word.charAt(i)-'a'];
+		}
+		return score;
+	}
+	
+	/***************************************************************/
+	/************************** Testing ****************************/
+	/***************************************************************/
+	public static void main (String [] args)
+	{
+		WordUtils wu = new WordUtils();
+		wu.run();
+	}
+	
+	public void run() {
+		String letters = Prompt.getString("Please enter a list of letters, from 3 to 12 letters long, without spaces");
+		loadWords();
+		String [] word = findAllWords(letters);
+		System.out.println();
+		printWords(word);
+		
+		// Score table in alphabetic order according to Scrabble
+		int [] scoreTable = {1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10};
+		String best = bestWord(word,scoreTable);
+		System.out.println("\nHighest scoring word: " + best + "\nScore = " 
+							+ getScore(best, scoreTable) + "\n");
+	}
+}
