e58f727df2724ab549153f1d6e4ecfea4bd687c8
Yoosuf-Khan-APCSA <ykhan977@student.fuhsd.org>
2024-10-23 15:04:26 -0700

    Add files via upload

--- Dice.java
+++ Dice.java
@@ -4,92 +4,38 @@
  *  the last roll value.
  *
  *  @author Mr Greenstein
- *  @version 2.0
  */
 
 public class Dice {
-	private int sides, rollcount, value;
+	private int sides, rollCount, rollValue;
 	
-	/* Create the seven different line images of a die */
-	private String [] line = {
-						" _______ ",
-						"|       |",
-						"| O   O |",
-						"|   O   |",
-						"|     O |",
-						"| O     |",
-						"|_______|" };
-	
-	/**	Constructor for Dice with s sides */
+	/*	 @param s	The number of sides for die */
 	public Dice (int s) {
 		sides = s;
-		rollcount = value = 0;
+		rollCount = rollValue = 0;
 	}
 	
-	/**	Constructor for default Dice with 6 sides */
+	/*	No-args constructor - default 6 sides */
 	public Dice ( ) {
 		this(6);
 	}
 	
-	/**	@return		the rolled value */
+	/**
+	 *  One roll of the die
+	 *
+	 *  @return		The value of the rolled die
+	 */
 	public int roll ( ) {
-		rollcount++;
-		value = (int)(Math.random() * sides) + 1;
-		return value;
+		rollCount++;
+		rollValue = (int)(Math.random()*sides) + 1;
+		return rollValue;
 	}
 	
-	/**	@return		number of rolls so far */
-	public int numRolls ( ) {
-		return rollcount;
-	}
+	/**	@return		number of rolls taken by this dice */
+	public int numRolls ( ) { return rollCount; }
 	
-	/**	@return		the value on the dice */
-	public int getValue ( ) {
-		return value;
-	}
-	
-	/**
-	 *  Prints out the images of the dice
-	 */
-	public void printDice() {		
-		/* Print each line */
-		for (int i = 0; i < 6; i++) {
-			System.out.print(" ");
-			printDiceLine(getValue() + 6 * i);
-			System.out.println();
-		}
-		System.out.println();
-	}
-	
-	/**
-	 *  @param value The index into the ASCII dice image
-	 */
-	private void printDiceLine(int value) {
-		System.out.print(line[getDiceLine(value)]);
-	}
-	
-	/**
-	 *  @param value The value of the die print line
-	 *  @return  The index into the ASCII dice image
-	 */
-	private int getDiceLine(int value) {
-		if (value < 7) return 0;
-		if (value < 14) return 1;
-		switch (value) {
-			case 20: case 22: case 25:
-				return 1;
-			case 16: case 17: case 18: case 24: case 28: case 29: case 30:
-				return 2;
-			case 19: case 21: case 23:
-				return 3;
-			case 14: case 15:
-				return 4;
-			case 26: case 27:
-				return 5;
-			default:	// value > 30
-				return 6;
-		}
-	}
+	/**	@return		the last rolled value */
+	public int getValue ( ) { return rollValue; }
 }
 
 
