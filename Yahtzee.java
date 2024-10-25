1eea4d9c7ae3a6689c8be15ebb42bfb85c330b33
Yoosuf-Khan-APCSA <ykhan977@student.fuhsd.org>
2024-10-24 21:10:05 -0700

    Add files via upload

--- Yahtzee.java
+++ Yahtzee.java
@@ -1,22 +1,147 @@
-	
-	public void printHeader() {
-		System.out.println("\n");
-		System.out.println("+------------------------------------------------------------------------------------+");
-		System.out.println("| WELCOME TO MONTA VISTA YAHTZEE!                                                    |");
-		System.out.println("|                                                                                    |");
-		System.out.println("| There are 13 rounds in a game of Yahtzee. In each turn, a player can roll his/her  |");
-		System.out.println("| dice up to 3 times in order to get the desired combination. On the first roll, the |");
-		System.out.println("| player rolls all five of the dice at once. On the second and third rolls, the      |");
-		System.out.println("| player can roll any number of dice he/she wants to, including none or all of them, |");
-		System.out.println("| trying to get a good combination.                                                  |");
-		System.out.println("| The player can choose whether he/she wants to roll once, twice or three times in   |");
-		System.out.println("| each turn. After the three rolls in a turn, the player must put his/her score down |");
-		System.out.println("| on the scorecard, under any one of the thirteen categories. The score that the     |");
-		System.out.println("| player finally gets for that turn depends on the category/box that he/she chooses  |");
-		System.out.println("| and the combination that he/she got by rolling the dice. But once a box is chosen  |");
-		System.out.println("| on the score card, it can't be chosen again.                                       |");
-		System.out.println("|                                                                                    |");
-		System.out.println("| LET'S PLAY SOME YAHTZEE!                                                           |");
-		System.out.println("+------------------------------------------------------------------------------------+");
-		System.out.println("\n\n");
+/**
+ *	Runs a game of Yahtzee
+ *	
+ *	@author	Yoosuf Khan
+ *	@since	October 23, 2024
+ */
+
+
+public class Yahtzee {
+
+    YahtzeePlayer player1 = new YahtzeePlayer();
+    YahtzeePlayer player2 = new YahtzeePlayer();
+
+	/**
+     * runs Yahtzee object
+     */
+    public static void main(String[] args) {
+        Yahtzee yz = new Yahtzee();
+        yz.runGame();
+    }
+
+	/**
+     * runs Yahtzee game
+     */
+    public void runGame() {
+        DiceGroup dice = new DiceGroup();
+
+        int player1Sum = 0, player2Sum = 0, choice = 0;
+        String hold = "";
+        boolean isValidChoice = false;
+        printHeader();
+        player1.setName(Prompt.getString("Player 1, please enter your first name"));
+        player2.setName(Prompt.getString("\nPlayer 2, please enter your first name"));
+		while(player1Sum==player2Sum){
+        Prompt.getString("\nLet's see who will go first. " + player1.getName()
+                + ", please hit enter to roll the dice");
+        dice.rollDice();
+        dice.printDice();
+        player1Sum = dice.getTotal();
+        Prompt.getString(player2.getName() + ", it's your turn. Please hit enter to roll the dice");
+        dice.rollDice();
+        dice.printDice();
+        player2Sum = dice.getTotal();
+        System.out.println(player1.getName() + ", you rolled a sum of " + player1Sum
+                + ", and " + player2.getName() + ", you rolled a sum of " + player2Sum + ". ");
+		if(player1Sum==player2Sum)
+			 System.out.println("Since you tied you will reroll.");
+		}
+        if (player1Sum > player2Sum) {
+            System.out.println(player1.getName() + ", since your sum was higher, you'll roll first. ");
+            for (int i = 0; i < 13; i++) {
+                runTurn(player1, true, i + 1);
+                runTurn(player2, false, i + 1);
+            }
+        } else {
+            System.out.println(player2.getName() + ", since your sum was higher, you'll roll first. ");
+            for (int i = 0; i < 13; i++) {
+                runTurn(player2, true, i + 1);
+                runTurn(player1, false, i + 1);
+            }
+        }
+		player1Sum=player2Sum=0;
+		for(int i=1;i<=13;i++){
+			player1Sum+=player1.getScoreCard().getScore(i);
+			player2Sum+=player2.getScoreCard().getScore(i);
+		}
+		player1.getScoreCard().printCardHeader();
+        player1.getScoreCard().printPlayerScore(player1);
+        player2.getScoreCard().printPlayerScore(player2);
+
+		System.out.printf("%n%-10s score total = %d%n",player1.getName(),player1Sum);
+		System.out.printf("%-10s score total = %d%n",player2.getName(),player2Sum);
+		if(player1Sum>player2Sum)
+			System.out.println("\nCongratulations "+player1.getName()+". YOU WON!!!\n");
+		else if (player1Sum<player2Sum)
+			System.out.println("\nCongratulations "+player2.getName()+". YOU WON!!!\n");
+		else
+			System.out.println(" YOU TIE!!!\n");
 	}
+
+	/**
+     * runs one turn of a player
+	 * @param  player the player the turn is for
+	 * @param  isFirst if the player is playing first
+	 * @param  round what round the turn is in
+     */
+    public void runTurn(YahtzeePlayer player, boolean isFirst, int round) {
+        String hold = "";
+        boolean isValidChoice = false;
+        int choice;
+        DiceGroup dice = new DiceGroup();
+        player1.getScoreCard().printCardHeader();
+        player1.getScoreCard().printPlayerScore(player1);
+        player2.getScoreCard().printPlayerScore(player2);
+        if (isFirst) {
+            System.out.println("Round " + (round) + " of 13 rounds.");
+        }
+        Prompt.getString("\n"+player.getName() + ", it's your turn to play. Please hit enter to roll the dice");
+        dice.rollDice();
+        dice.printDice();
+        for (int j = 0; j < 2 && !hold.equals("-1"); j++) {
+            hold = Prompt.getString("\nWhich di(c)e would you like to keep? Enter the values you'd like to 'hold' without\n"
+                    + "spaces. For examples, if you'd like to 'hold' die 1, 2, and 5, enter 125"+
+					"\n(enter -1 if you'd like to end the turn)");
+            if (!hold.equals("-1")) {
+                dice.rollDice(hold);
+                dice.printDice();
+            }
+        }
+        player1.getScoreCard().printCardHeader();
+        player1.getScoreCard().printPlayerScore(player1);
+        player2.getScoreCard().printPlayerScore(player2);
+        System.out.printf("  NAME\t\t  1    2    3    4    5    6   7    8    9   "
+                + "10   11   12   13  \n\n");
+        isValidChoice = false;
+        while (!isValidChoice) {
+            choice = Prompt.getInt(player.getName() + ", now you need to make a choice. "
+                    + "Pick a valid integer from the list above ", 1, 13);
+            isValidChoice = player.getScoreCard().changeScore(choice, dice);
+        }
+    }
+
+	/**
+     * prints Yahtzee header
+     */
+    public void printHeader() {
+        System.out.println("\n");
+        System.out.println("+------------------------------------------------------------------------------------+");
+        System.out.println("| WELCOME TO MONTA VISTA YAHTZEE!                                                    |");
+        System.out.println("|                                                                                    |");
+        System.out.println("| There are 13 rounds in a game of Yahtzee. In each turn, a player can roll his/her  |");
+        System.out.println("| dice up to 3 times in order to get the desired combination. On the first roll, the |");
+        System.out.println("| player rolls all five of the dice at once. On the second and third rolls, the      |");
+        System.out.println("| player can roll any number of dice he/she wants to, including none or all of them, |");
+        System.out.println("| trying to get a good combination.                                                  |");
+        System.out.println("| The player can choose whether he/she wants to roll once, twice or three times in   |");
+        System.out.println("| each turn. After the three rolls in a turn, the player must put his/her score down |");
+        System.out.println("| on the scorecard, under any one of the thirteen categories. The score that the     |");
+        System.out.println("| player finally gets for that turn depends on the category/box that he/she chooses  |");
+        System.out.println("| and the combination that he/she got by rolling the dice. But once a box is chosen  |");
+        System.out.println("| on the score card, it can't be chosen again.                                       |");
+        System.out.println("|                                                                                    |");
+        System.out.println("| LET'S PLAY SOME YAHTZEE!                                                           |");
+        System.out.println("+------------------------------------------------------------------------------------+");
+        System.out.println("\n\n");
+    }
+}
--- YahtzeeScoreCard.java
+++ YahtzeeScoreCard.java
@@ -1,71 +1,251 @@
-	public class YahtzeeScoreCard{
-		
-	/**
-	 *  Print the scorecard header
-	 */
-	public void printCardHeader() {
-		System.out.println("\n");
-		System.out.printf("\t\t\t\t\t       3of  4of  Fll Smll Lrg\n");
-		System.out.printf("  NAME\t\t  1    2    3    4    5    6   Knd  Knd  Hse " +
-						"Strt Strt Chnc Ytz!\n");
-		System.out.printf("+----------------------------------------------------" +
-						"---------------------------+\n");
-	}
-	
-	/**
-	 *  Prints the player's score
-	 */
-	public void printPlayerScore(YahtzeePlayer player) {
-		System.out.printf("| %-12s |", player.getName());
-		for (int i = 1; i < 14; i++) {
-			if (getScore(i) > -1)
-				System.out.printf(" %2d |", getScore(i));
-			else System.out.printf("    |");
-		}
-		System.out.println();
-		System.out.printf("+----------------------------------------------------" +
-						"---------------------------+\n");
-	}
 
+public class YahtzeeScoreCard {
+
+    int scores[]; // scores for score cards 1-13
+    private final int NUM_DICE = 5;	// number of dice
+    private final int NUM_SCORE_HOLES = 13;	// number of score pegs
+    int diceValues[]; //array with dice values that were rolled
 
 	/**
-	 *  Change the scorecard based on the category choice 1-13.
-	 *
-	 *  @param choice The choice of the player 1 to 13
-	 *  @param dg  The DiceGroup to score
-	 *  @return  true if change succeeded. Returns false if choice already taken.
-	 */
-	public boolean changeScore(int choice, DiceGroup dg) {}
-	
+     * initialize scores and dice values
+     */
+    public YahtzeeScoreCard() {
+        scores = new int[NUM_SCORE_HOLES];
+        diceValues = new int[6];
+        for (int i = 0; i < scores.length; i++) {
+            scores[i] = -1;
+        }
+    }
+
+    /**
+     * Print the scorecard header
+     */
+    public void printCardHeader() {
+        System.out.println("\n");
+        System.out.printf("\t\t\t\t\t       3of  4of  Fll Smll Lrg\n");
+        System.out.printf("  NAME\t\t  1    2    3    4    5    6   Knd  Knd  Hse "
+                + "Strt Strt Chnc Ytz!\n");
+        System.out.printf("+----------------------------------------------------"
+                + "---------------------------+\n");
+    }
+
+    /**
+     * Prints the player's score
+     */
+    public void printPlayerScore(YahtzeePlayer player) {
+        System.out.printf("| %-12s |", player.getName());
+        for (int i = 1; i < 14; i++) {
+            if (getScore(i) > -1) {
+                System.out.printf(" %2d |", getScore(i)); 
+            }else {
+                System.out.printf("    |");
+            }
+        }
+        System.out.println();
+        System.out.printf("+----------------------------------------------------"
+                + "---------------------------+\n");
+    }
+
+    /**
+     * Change the scorecard based on the category choice 1-13.
+     *
+     * @param choice The choice of the player 1 to 13
+     * @param dg The DiceGroup to score
+     * @return true if change succeeded. Returns false if choice already taken.
+     */
+    public boolean changeScore(int choice, DiceGroup dg) {
+        if (getScore(choice) > -1) {
+            return false; 
+        }else {
+            switch (choice) {
+                case 7:
+                    threeOfAKind(dg);
+                    break;
+                case 8:
+                    fourOfAKind(dg);
+                    break;
+                case 9:
+                    fullHouse(dg);
+                    break;
+                case 10:
+                    smallStraight(dg);
+                    break;
+                case 11:
+                    largeStraight(dg);
+                    break;
+                case 12:
+                    chance(dg);
+                    break;
+                case 13:
+                    yahtzeeScore(dg);
+                    break;
+                default:
+                    numberScore(choice, dg);
+                    break;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Change the scorecard for a number score 1 to 6
+     *
+     * @param choice The choice of the player 1 to 6
+     * @param dg The DiceGroup to score
+     */
+    public void numberScore(int choice, DiceGroup dg) {
+        initializeDiceValues(dg);
+        scores[choice - 1] = diceValues[choice - 1] * choice;
+    }
+
+    /**
+     * Updates the scorecard for Three Of A Kind choice.
+     *
+     * @param dg	The DiceGroup to score
+     */
+    public void threeOfAKind(DiceGroup dg) {
+        initializeDiceValues(dg);
+        boolean has3Kind = false;
+        int winningDiceFace = 0;
+        for (int i = 0; i < diceValues.length; i++) {
+            if (diceValues[i] >= 3) {
+                has3Kind = true;
+                winningDiceFace = i + 1;
+            }
+        }
+        if (has3Kind) {
+            scores[6] = 3 * winningDiceFace; 
+        }else {
+            scores[6] = 0;
+        }
+    }
 	/**
-	 *  Change the scorecard for a number score 1 to 6
-	 *
-	 *  @param choice The choice of the player 1 to 6
-	 *  @param dg  The DiceGroup to score
-	 */
-	public void numberScore(int choice, DiceGroup dg) {}
-	
+     * Updates the scorecard for Four Of A Kind choice.
+     *
+     * @param dg	The DiceGroup to score
+     */
+    public void fourOfAKind(DiceGroup dg) {
+        initializeDiceValues(dg);
+        boolean has4Kind = false;
+        int winningDiceFace = 0;
+        for (int i = 0; i < diceValues.length; i++) {
+            if (diceValues[i] >= 4) {
+                has4Kind = true;
+                winningDiceFace = i + 1;
+            }
+        }
+        if (has4Kind) {
+            scores[7] = 4 * winningDiceFace; 
+        }else {
+            scores[7] = 0;
+        }
+    }
+
 	/**
-	 *	Updates the scorecard for Three Of A Kind choice.
-	 *
-	 *	@param dg	The DiceGroup to score
-	 */	
-	public void threeOfAKind(DiceGroup dg) {}
-	
-	public void fourOfAKind(DiceGroup dg) {}
-	
-	public void fullHouse(DiceGroup dg) {}
-	
-	public void smallStraight(DiceGroup dg) {}
-	
-	public void largeStraight(DiceGroup dg) {}
-	
-	public void chance(DiceGroup dg) {
-		
-		}
-	
-	public void yahtzeeScore(DiceGroup dg) {
-		for(int i=0; i<)
-		if(dg.getDiceArray[i])
-	}
+     * Updates the scorecard for Full House choice.
+     *
+     * @param dg	The DiceGroup to score
+     */
+    public void fullHouse(DiceGroup dg) {
+        initializeDiceValues(dg);
+        boolean hasPair = false, hasTriple = false;
+        int winningDiceFace = 0;
+        for (int i = 0; i < diceValues.length; i++) {
+            if (diceValues[i] == 2) {
+                hasPair = true;
+            }
+            if (diceValues[i] == 3) {
+                hasTriple = true;
+            }
+        }
+        if (hasPair && hasTriple) {
+            scores[8] = 25; 
+        }else {
+            scores[8] = 0;
+        }
+
+    }
+
+	/**
+     * Updates the scorecard for Small Straight choice.
+     *
+     * @param dg	The DiceGroup to score
+     */
+    public void smallStraight(DiceGroup dg) {
+        initializeDiceValues(dg);
+        if (diceValues[0] > 0 && diceValues[1] > 0 && diceValues[2] > 0 && diceValues[3] > 0) {
+            scores[9] = 30; 
+        }else if (diceValues[1] > 0 && diceValues[2] > 0 && diceValues[3] > 0 && diceValues[4] > 0) {
+            scores[9] = 30; 
+        }else if (diceValues[2] > 0 && diceValues[3] > 0 && diceValues[4] > 0 && diceValues[5] > 0) {
+            scores[9] = 30; 
+        }else {
+            scores[9] = 0;
+        }
+    }
+	/**
+     * Updates the scorecard for Large Straight choice.
+     *
+     * @param dg	The DiceGroup to score
+     */
+    public void largeStraight(DiceGroup dg) {
+        initializeDiceValues(dg);
+        if (diceValues[0] > 0 && diceValues[1] > 0 && diceValues[2] > 0 && diceValues[3] > 0 && diceValues[4] > 0) {
+            scores[10] = 40; 
+        }else if (diceValues[1] > 0 && diceValues[2] > 0 && diceValues[3] > 0 && diceValues[4] > 0 && diceValues[5] > 0) {
+            scores[10] = 40; 
+        }else {
+            scores[10] = 0;
+        }
+    }
+	/**
+     * Updates the scorecard for Chance choice.
+     *
+     * @param dg	The DiceGroup to score
+     */
+    public void chance(DiceGroup dg) {
+        scores[11] = dg.getTotal();
+    }
+	/**
+     * Updates the scorecard for Yahtzee choice.
+     *
+     * @param dg	The DiceGroup to score
+     */
+    public void yahtzeeScore(DiceGroup dg) {
+        initializeDiceValues(dg);
+        boolean hasYahtzee = false;
+        for (int i = 0; i < diceValues.length; i++) {
+            if (diceValues[i] == 5) {
+                hasYahtzee = true;
+            }
+        }
+        if (hasYahtzee) {
+            scores[12] = 50; 
+        }else {
+            scores[12] = 0;
+        }
+    }
+	/**
+     * returns the score of the category selected
+	 * 
+     * @param pointCategory	The catagory of the score requested
+	 * @return score of said category
+     */
+    public int getScore(int pointCategory) {
+        return scores[pointCategory - 1];
+    }
+	/**
+     * Initializes the values of diceValue to match the number of occurences of each face in dg
+     *
+     * @param dg	The DiceGroup to score
+     */
+    public void initializeDiceValues(DiceGroup dg) {
+        for (int i = 0; i < diceValues.length; i++) {
+            diceValues[i] = 0;
+        }
+        System.out.println();
+        for (int i = 0; i < NUM_DICE; i++) {
+            diceValues[dg.getDice(i) - 1]++;;
+        }
+    }
 }
