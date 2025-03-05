cbe7e6887c0fb38fbf68097ae1ed26c55e82c32f
Yoosuf-Khan-APCSA <ykhan977@student.fuhsd.org>
2025-03-05 14:38:30 -0800

    Add files via upload

---
+++ zero.java
@@ -0,0 +1,18 @@
+	public class zero{
+	public static void main (String [] args){
+		Double num = Prompt.getDouble("");
+		System.out.printf("%s%n",TruncatedZero(num));
+		}
+	public static String TruncatedZero(double num){
+		String str = num+"";
+		for (int i = str.length()-1; i>str.indexOf('.');i--){
+			if(str.indexOf('.')+1==i){}
+			else{
+				if(str.charAt(i)=='0')
+				str= str.substring(0,i);
+				else i=0;
+			}
+		}
+		return str;
+	}
+}
